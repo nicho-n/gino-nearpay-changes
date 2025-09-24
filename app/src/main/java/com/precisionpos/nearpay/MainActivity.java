21676d4816696c62a09aa251f84a97ed52229871
Nicholas Nowak <nicholaspnowak@gmail.com>
2025-09-25 01:01:20 +0800

    initial commit

---
+++ app/src/main/java/com/precisionpos/nearpay/MainActivity.java
@@ -0,0 +1,42 @@
+package com.precisionpos.nearpay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+import com.precisionpos.nearpay.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            TerminalSDK nearpay = new TerminalSDK.Builder()
+                    .activity(this)  // Sets the activity context
+                    .environment(SdkEnvironment.SANDBOX)  // Choose SANDBOX or PRODUCTION or INTERNAL
+                    .googleCloudProjectNumber(12345678L)  // Set Google Cloud project number
+                    .huaweiSafetyDetectApiKey("your_api_key")  // Set Huawei API key
+                    .country(Country.SA)  // Set country SA, TR, USA, KEN
+                    .build();
+        } catch (Throwable e) {
+            Timber.e("Error initializing TerminalSDK: $e");
+        }
+
+    }
+
+
+
+
+}
